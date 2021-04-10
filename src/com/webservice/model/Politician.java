94b49f8206bef4ef2a4aee7b6f73ba82b38c4701
farah <farah-manoubi@users.noreply.github.com>
2021-04-10 20:33:24 +0200

    Création des classes modeles

---
+++ src/com/webservice/model/Politician.java
@@ -0,0 +1,27 @@
+package com.webservice.model;
+
+public class Politician {
+	private String name;
+	private int age;
+	private int popularity;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPopularity() {
+		return popularity;
+	}
+	public void setPopularity(int popularity) {
+		this.popularity = popularity;
+	}
+}
--- src/dmServiceWS/Test.java
+++
@@ -1,5 +0,0 @@
-package dmServiceWS;
-
-public class Test {
-//test
-}
